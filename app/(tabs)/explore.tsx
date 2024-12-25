b6f09c974db61755ffa48ec944854354cbf5a17f
Mukelani Mashoba <mukelanilastborn@gmail.com>
2024-12-25 06:26:01 +0200

    new

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,15 +1,29 @@
-import { StyleSheet, Image, Platform, Text, View } from 'react-native';
+import { StyleSheet, Image, Platform, Text, View, TextInput } from 'react-native';
 import { IconSymbol } from '@/components/ui/IconSymbol';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function TabTwoScreen() {
   return (
-    <View>
+    <View style={styles.container}>
+
+      <View>
+        <Ionicons name='search-circle-outline' size={20} />
+        <TextInput
+          placeholder='search content'
+        />
+      </View>
+
+
       <Text>Explore</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
   headerImage: {
     color: '#808080',
     bottom: -90,
