784b5f139e2ff4741fc04a4729f55639486f47a8
Mukelani Mashoba <mukelanilastborn@gmail.com>
2025-01-10 20:03:45 +0200

    setting ui done and configured to send msg to whatsapp

--- app/(tabs)/setting.tsx
+++ app/(tabs)/setting.tsx
@@ -1,7 +1,7 @@
 import HeaderContainer from '@/components/header'
 import { Ionicons } from '@expo/vector-icons';
 import React, { useState } from 'react'
-import { ImageBackground, Linking, Platform, StyleSheet, Switch, Text, TouchableOpacity, View } from 'react-native'
+import { ImageBackground, Linking, Platform, ScrollView, StyleSheet, Switch, Text, TouchableOpacity, View } from 'react-native'
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 export default function setting() {
@@ -15,8 +15,8 @@ export default function setting() {
     }
 
     const sendWhatsApp = () => {
-        let msg = "type something";
-        let phoneWithCountryCode = "xxxxxxxxxx";
+        let msg = "Hello, I am sending the feddback from the ESMS app I enjoy it so much";
+        let phoneWithCountryCode = "+27 638619542";
 
         let mobile =
             Platform.OS == "ios" ? phoneWithCountryCode : "+" + phoneWithCountryCode;
@@ -47,104 +47,103 @@ export default function setting() {
                     <HeaderContainer />
                 </View>
 
-                <View style={styles.banner}>
-                    <ImageBackground source={require("../../assets/pictures/img3.jpg")} style={{ width: '100%', height: 200, borderRadius: 20, overflow: 'hidden', justifyContent: 'center', alignItems: 'center' }}>
-                        <View style={styles.photoContainer}>
-                            <Text style={{ color: "white", fontWeight: "300", fontSize: 18, fontFamily: "Roboto-Regular" }}>Let's Learn Together</Text>
-                        </View>
-                    </ImageBackground>
-                </View>
-
-
-                <View>
-                    {/* Appearance */}
-                    <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                        <Ionicons name='settings-outline' size={30} color={"lightblue"} />
-                        <View>
-                            <Text>Appearance</Text>
-                            <Text>Make your app experience your own by customizing your profile</Text>
-                        </View>
-                        <TouchableOpacity>
-                            <Ionicons name='arrow-forward-outline' size={24} />
-                        </TouchableOpacity>
+                <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{ flexGrow: 1, gap: 10, marginBottom: 10 }}>
+                    <View style={styles.banner}>
+                        <ImageBackground source={require("../../assets/pictures/img2.png")} style={{ width: '100%', height: 240, borderRadius: 20, overflow: 'hidden', justifyContent: 'center', alignItems: 'center' }}>
+                            <View style={styles.photoContainer}>
+                                <Text style={{ color: "white", fontWeight: "300", fontSize: 18, fontFamily: "Roboto-Regular" }}>Let's Learn Together</Text>
+                            </View>
+                        </ImageBackground>
                     </View>
 
-                    {/* Privacy */}
-                    <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                        <Ionicons name='settings-outline' size={30} color={"purple"} />
-                        <View>
-                            <Text>Dark Mode</Text>
-                            <Text>Automatic</Text>
+
+                    <View>
+                        {/* Appearance */}
+                        <View style={styles.appearance}>
+                            <Ionicons name='settings-outline' size={30} color={"lightblue"} />
+                            <View>
+                                <Text>Appearance</Text>
+                                <Text>Make these app yours</Text>
+                            </View>
+                            <TouchableOpacity>
+                                <Ionicons name='arrow-forward-circle-outline' size={24} color={"grey"} />
+                            </TouchableOpacity>
                         </View>
-                        <Switch
-                            trackColor={{ false: '#767577', true: '#81b0ff' }}
-                            thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
-                            ios_backgroundColor="#3e3e3e"
-                            onValueChange={toggleSwitch}
-                            value={isEnabled}
-                        />
+
+                        {/* Privacy */}
+                        <View style={styles.appearance}>
+                            <Ionicons name='settings-outline' size={30} color={"purple"} />
+                            <View>
+                                <Text>Dark Mode</Text>
+                                <Text>Light or dark mode</Text>
+                            </View>
+                            <Switch
+                                trackColor={{ false: '#767577', true: '#B4F8C8' }}
+                                thumbColor={isEnabled ? '#B4F8C8' : '#f4f3f4'}
+                                ios_backgroundColor="#3e3e3e"
+                                onValueChange={toggleSwitch}
+                                value={isEnabled}
+                            />
+                        </View>
+
+                        {/* Dark Mode */}
+                        <View style={styles.appearance}>
+                            <Ionicons name='finger-print-sharp' size={34} color={"orange"} />
+                            <View>
+                                <Text>Privacy</Text>
+                                <Text>Check our privacy policy</Text>
+                            </View>
+                            <TouchableOpacity>
+                                <Ionicons name='arrow-forward-circle-outline' size={24} color={"grey"} />
+                            </TouchableOpacity>
+                        </View>
+
+                        {/* About */}
+                        <View style={styles.appearance}>
+                            <Ionicons name='information-circle-sharp' size={37} color={"purple"} />
+                            <View>
+                                <Text>About</Text>
+                                <Text>MGM Group and ESMS</Text>
+                            </View>
+                            <TouchableOpacity>
+                                <Ionicons name='arrow-forward-circle-outline' size={24} color={"grey"} />
+                            </TouchableOpacity>
+                        </View>
+
+                        {/* Send Feedbacks */}
+                        <View style={styles.appearance}>
+                            <Ionicons name='chatbubble-ellipses-sharp' size={30} color={"lightgreen"} />
+                            <View>
+                                <Text>Send Feedback</Text>
+                                <Text>Tell us your experience</Text>
+                            </View>
+                            <TouchableOpacity onPress={sendWhatsApp}>
+                                <Ionicons name='arrow-forward-circle-outline' size={24} color={"grey"} />
+                            </TouchableOpacity>
+                        </View>
                     </View>
 
-                    {/* Dark Mode */}
-                    <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                        <Ionicons name='finger-print-sharp' size={30} color={"orange"} />
-                        <View>
-                            <Text>Privacy</Text>
-                            <Text>Check all our privacy policy</Text>
+
+                    {/* Account Section still workings*/}
+                    <Text style={{fontSize: 22}}>Account</Text>
+                    <View style={styles.account}>
+                        <View style={{ width: "45%" }}>
+                            <TouchableOpacity onPress={HandleLogout} style={styles.logoutBtn}>
+                                <Ionicons name='log-out-outline' size={24} color={"black"} />
+                                <Text style={{color: "white", fontFamily: "Roboto-regular"}}>Sign Out</Text>
+                            </TouchableOpacity>
                         </View>
-                        <TouchableOpacity>
-                            <Ionicons name='arrow-forward-outline' size={24} />
-                        </TouchableOpacity>
+
+
+                        <View style={{ width: "45%" }}>
+                            <TouchableOpacity style={styles.changeEmail}>
+                                <Ionicons name='return-up-forward-outline' size={24} color='black' />
+                                <Text>Change Email</Text>
+                            </TouchableOpacity>
+                        </View>
                     </View>
 
-                    {/* About */}
-                    <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                        <Ionicons name='information-sharp' size={30} color={"purple"} />
-                        <View>
-                            <Text>About</Text>
-                            <Text>Learn more about us as MGM Group and ESMS</Text>
-                        </View>
-                        <TouchableOpacity>
-                            <Ionicons name='arrow-forward-outline' size={24} />
-                        </TouchableOpacity>
-                    </View>
-
-                    {/* Send Feedbacks */}
-                    <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                        <Ionicons name='chatbubble-sharp' size={30} color={"green"} />
-                        <View>
-                            <Text>Send Feedback</Text>
-                            <Text>Let us know how can we make ESMS better</Text>
-                        </View>
-                        <TouchableOpacity onPress={sendWhatsApp}>
-                            <Ionicons name='arrow-forward-outline' size={24} />
-                        </TouchableOpacity>
-                    </View>
-                </View>
-
-
-                {/* Account Section still workings*/}
-                <Text>Account</Text>
-
-                <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                    <TouchableOpacity onPress={HandleLogout} style={styles.logoutBtn}>
-                        <Ionicons name='arrow-forward-outline' size={24} />
-                        <Text>Sign Out</Text>
-                    </TouchableOpacity>
-                </View>
-
-
-                <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                    <Ionicons name='return-up-forward-outline' size={24} color='black' />
-                    <Text>Change Email</Text>
-                    <TouchableOpacity>
-                        <Ionicons name='arrow-forward-outline' size={24} color='black' />
-                    </TouchableOpacity>
-                </View>
-
-
-
-                <Text>Settings</Text>
+                </ScrollView>
             </SafeAreaView>
         </SafeAreaProvider>
     )
@@ -153,24 +152,58 @@ export default function setting() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "white",
+        backgroundColor: "#f5f5f5",
         padding: 10,
+        gap: 10
     },
     logoutBtn: {
-
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        backgroundColor: "#E3242B",
+        padding: 12,
+        borderRadius: 10,
+        marginBottom: 10,
+    },
+    changeEmail: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        backgroundColor: "#B4F8C8",
+        padding: 12,
+        borderRadius: 10,
+        marginBottom: 10,
     },
     banner: {
-        // flexDirection: 'row',
-        // justifyContent: 'space-evenly',
         alignItems: 'center',
-        marginBottom: 40,
+        marginBottom: 10,
     },
     photoContainer: {
-        // backgroundColor: 'lightgrey',
         padding: 5,
         borderRadius: 20,
         height: "20%",
         width: "100%",
     },
+    appearance: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        backgroundColor: "white",
+        padding: 12,
+        borderRadius: 10,
+        marginBottom: 10,
+        alignItems: 'center',
+    },
+    account: {
+        backgroundColor: "white",
+        padding: 12,
+        borderRadius: 10,
+        marginBottom: 35,
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        width: "100%",
+        flexDirection: 'row',
+        gap: 5,
+    },
+
 
 })
