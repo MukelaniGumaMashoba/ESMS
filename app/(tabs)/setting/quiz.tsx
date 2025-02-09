349066ee68e99a0167b5914ffef6f1d5b3136e71
Mukelani Mashoba <mukelanilastborn@gmail.com>
2025-02-10 00:40:10 +0200

    Adding a quiz on the settings page

---
+++ app/(tabs)/setting/quiz.tsx
@@ -0,0 +1,11 @@
+import Quiz from '@/components/quiz/Main'
+import React from 'react'
+import { View } from 'react-native'
+
+export default function quiz() {
+    return (
+        <View style={{flex: 1}}>
+            <Quiz />
+        </View>
+    )
+}
